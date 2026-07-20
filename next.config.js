935f6fd3f713a76f10204341efa42be364b87c11
roliveira24 <80493232+roliveira24@users.noreply.github.com>
2026-07-20 16:54:06 +0200

    Configure Next.js for GitHub Pages static export

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',
+  basePath: '/portfolio',
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
